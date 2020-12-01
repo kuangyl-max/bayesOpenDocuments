74e4f4efca53fb951c8278c96851649e81d53544
kuangyl-max <15346514521@163.com>
2020-12-01 17:05:16 +0800

    refactor .zip

--- rm-zip-pdf-git.sh
+++ rm-zip-pdf-git.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm -f diagram.zip || echo ''
 zip -e diagram.zip *.pdf && git checkout kyl && git add diagram.zip && git commit -m "diagram.zip" && git push origin kyl:master
-#12341
+#buzhidao2333
